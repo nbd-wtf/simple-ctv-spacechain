755c7c9e05b3b877ea6a3e10dad787f52a9bc6f0
fiatjaf <fiatjaf@gmail.com>
2022-04-24 13:12:22 -0300

    generating the string of transactions and pretty printing it.

---
+++ main.py
@@ -0,0 +1,157 @@
+import sys
+import pprint
+import argparse
+from dataclasses import dataclass
+
+from bitcoin.core import (
+    CTransaction,
+    CMutableTransaction,
+    CTxIn,
+    CTxOut,
+    CScript,
+    CScriptOp,
+    COutPoint,
+    CTxWitness,
+    CTxInWitness,
+    CScriptWitness,
+    COIN,
+)
+from bitcoin.core import script
+from bitcoin.wallet import CBech32BitcoinAddress
+from buidl.hd import HDPrivateKey, PrivateKey
+from buidl.ecc import S256Point
+from rpc import BitcoinRPC, JSONRPCError
+from utils import *
+
+CHAIN_MAX = 4
+SATS_AMOUNT = 1000
+OP_CHECKTEMPLATEVERIFY = script.OP_NOP4
+anyone_can_spend = CScript([script.OP_TRUE])
+
+colors = {
+    0: {
+        "prevout": lambda x: magenta(x),
+        "txid": lambda x: bold(cyan(x)),
+    },
+    1: {
+        "prevout": lambda x: cyan(x),
+        "txid": lambda x: bold(green(x)),
+    },
+    2: {
+        "prevout": lambda x: green(x),
+        "txid": lambda x: bold(blue(x)),
+    },
+    3: {
+        "prevout": lambda x: blue(x),
+        "txid": lambda x: bold(magenta(x)),
+    },
+}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("seed")
+    argv = parser.parse_args(sys.argv)
+
+    print(f'> generating transactions for spacechain "{argv.seed}"...')
+    txs = pregenerate_transactions(argv.seed)
+    for i in range(len(txs)):
+        txid_color = colors[i % len(colors)]["txid"]
+        prevout_color = colors[i % len(colors)]["prevout"]
+        ctv_color = lambda x: yellow(x)
+
+        tx = txs[i]
+        shorten_txid = txid_color(shorten(bytes_to_txid(tx.GetTxid())))
+        shorten_ctv_hash = ctv_color(shorten(get_standard_template_hash(tx, 0).hex()))
+        prevout_0_txid, prevout_0_n = str(tx.vin[0].prevout).split(":")
+        shorten_prevout_0 = ":".join(
+            [prevout_color(shorten(prevout_0_txid)), prevout_0_n]
+        )
+        redeem_script_0 = italic(
+            " ".join(
+                [
+                    "OP_CHECKTEMPLATEVERIFY"
+                    if el == CScriptOp(0xB3)
+                    else ctv_color(shorten(el.hex()))
+                    for el in tx.vin[0].scriptSig
+                ]
+            )
+        )
+        outscript_0 = white(format_cscript(tx.vout[0].scriptPubKey))
+        outscript_1 = white(format_cscript(tx.vout[1].scriptPubKey))
+        print(bold(f"tx {i+1} ~"))
+        print(f"  id: {bold(shorten_txid)}")
+        print(f"  ctvhash: {shorten_ctv_hash}")
+        print(f"    vin[0] = [{shorten_prevout_0}] {redeem_script_0}")
+        print(f"    vin[1] = empty")
+        print(f"    vout[0] = {italic(tx.vout[0].nValue)}sat 🠖 {outscript_0}")
+        print(f"    vout[1] = {italic(tx.vout[1].nValue)}sat 🠖 {outscript_1}")
+
+
+def pregenerate_transactions(seed):
+    txs_reversed = []
+    last = CMutableTransaction()
+    last.nVersion = 2
+    last.vin = [CTxIn()]  # blank because CTV in p2wsh
+    last.vout = [
+        CTxOut(
+            SATS_AMOUNT,
+            # the chain of transactions ends here with an OP_RETURN
+            CScript([script.OP_RETURN, seed.encode("utf-8")]),
+        )
+    ]
+    txs_reversed.append(last)
+    next = last
+
+    for i in range(CHAIN_MAX):
+        redeem_script = CScript(
+            [
+                get_standard_template_hash(next, 0),  # ctv hash
+                OP_CHECKTEMPLATEVERIFY,
+            ]
+        )
+
+        prev = CMutableTransaction()
+        prev.nVersion = 2
+        prev.vin = [CTxIn(), CTxIn()]  # blank because CTV in p2wsh uses blank here
+        prev.vout = [
+            # this output continues the transaction chain
+            CTxOut(
+                SATS_AMOUNT,
+                # standard p2wsh output:
+                CScript([script.OP_0, sha256(redeem_script)]),
+            ),
+            # this output is used for CPFP and for the miner to include the hash
+            CTxOut(
+                SATS_AMOUNT * 3,
+                # standard p2wsh output:
+                CScript([script.OP_0, sha256(anyone_can_spend)]),
+            ),
+        ]
+
+        txs_reversed.append(prev)
+        next = prev
+
+    # reverse the list so we start with the first transaction
+    # and also add the correct inputs
+    txs = [txs_reversed[-1]]
+    for i in range(len(txs_reversed) - 2, 0, -1):
+        this = txs_reversed[i]
+        next = txs_reversed[i + 1]
+        this.vin[0] = CTxIn(
+            COutPoint(next.GetTxid(), 0),
+            # redeem_script
+            CScript(
+                [
+                    get_standard_template_hash(this, 0),  # ctv hash
+                    OP_CHECKTEMPLATEVERIFY,
+                ]
+            ),
+        )
+        txs.append(this)
+
+    return txs
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,108 @@
+import struct
+import hashlib
+from bitcoin.core import CTransaction, COutPoint
+from bitcoin.core.script import CScript, OPCODE_NAMES
+
+
+def log(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def sha256(s) -> bytes:
+    return hashlib.sha256(s).digest()
+
+
+def ser_compact_size(l) -> bytes:
+    r = b""
+    if l < 253:
+        r = struct.pack("B", l)
+    elif l < 0x10000:
+        r = struct.pack("<BH", 253, l)
+    elif l < 0x100000000:
+        r = struct.pack("<BI", 254, l)
+    else:
+        r = struct.pack("<BQ", 255, l)
+    return r
+
+
+def ser_string(s) -> bytes:
+    return ser_compact_size(len(s)) + s
+
+
+def get_standard_template_hash(tx: CTransaction, nIn: int) -> bytes:
+    r = b""
+    r += struct.pack("<i", tx.nVersion)
+    r += struct.pack("<I", tx.nLockTime)
+    vin = tx.vin or []
+    vout = tx.vout or []
+    for i in range(len(vin)):
+        inp = vin[i]
+        if inp.scriptSig and inp.scriptSig.is_p2sh():
+            print(i, inp.scriptSig.is_p2sh())
+            r += sha256(ser_string(inp.scriptSig))
+    r += struct.pack("<I", len(tx.vin))
+    r += sha256(b"".join(struct.pack("<I", inp.nSequence) for inp in vin))
+    r += struct.pack("<I", len(tx.vout))
+    r += sha256(b"".join(out.serialize() for out in vout))
+    r += struct.pack("<I", nIn)
+    return sha256(r)
+
+
+def format_cscript(script: CScript) -> str:
+    return " ".join([str(el) if el in OPCODE_NAMES else el.hex() for el in script])
+
+
+def txid_to_bytes(txid: str) -> bytes:
+    """Convert the txids output by Bitcoin Core (little endian) to bytes."""
+    return bytes.fromhex(txid)[::-1]
+
+
+def bytes_to_txid(b: bytes) -> str:
+    """Convert big-endian bytes to Core-style txid str."""
+    return b[::-1].hex()
+
+
+def shorten(s: str) -> str:
+    return s[0:4] + "…" + s[-3:]
+
+
+def to_outpoint(txid: str, n: int) -> COutPoint:
+    return COutPoint(txid_to_bytes(txid), n)
+
+
+def scan_utxos(rpc, addr):
+    return rpc.scantxoutset("start", [f"addr({addr})"])
+
+
+def make_color(start, end):
+    def color_func(s):
+        return start + t_(str(s)) + end
+
+    return color_func
+
+
+def esc(*codes) -> str:
+    """
+    Produces an ANSI escape code from a list of integers
+    """
+    return t_("\x1b[{}m").format(t_(";").join(t_(str(c)) for c in codes))
+
+
+def t_(b) -> str:
+    """ensure text type"""
+    if isinstance(b, bytes):
+        return b.decode()
+    return b
+
+
+FG_END = esc(39)
+red = make_color(esc(31), FG_END)
+green = make_color(esc(32), FG_END)
+yellow = make_color(esc(33), FG_END)
+blue = make_color(esc(34), FG_END)
+magenta = make_color(esc(35), FG_END)
+cyan = make_color(esc(36), FG_END)
+white = make_color(esc(37), FG_END)
+bold = make_color(esc(1), esc(22))
+italic = make_color(esc(3), esc(23))
+underline = make_color(esc(4), esc(24))
